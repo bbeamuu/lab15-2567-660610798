f10678d195fbcffb39c792555010693e3ab258ce
Beam <b3bb0z@gmail.com>
2024-09-12 00:23:29 +0700

    late

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import TermsAndCondsModal from "@components/TermsAndCondsModal";
@@ -22,7 +23,7 @@ import {
   Title,
 } from "@mantine/core";
 
-import { Form, useForm, zodResolver } from "@mantine/form";
+import { useForm, zodResolver } from "@mantine/form";
 import { z } from "zod";
 
 const schema = z
@@ -35,39 +36,39 @@ const schema = z
       .min(3, { message: "Last name must have at least 3 characters" }),
     email: z.string().email({ message: "Invalid email format" }),
     plan: z.enum(["funrun", "mini", "half", "full"], {
-      errorMap: (issue, ctx) => ({ message: "Please select a plan" }),
+      errorMap: () => ({ message: "Please select a plan" }),
     }),
     gender: z.enum(["male", "female"], {
-      errorMap: (issue, ctx) => ({ message: "Please choose a gender" }),
+      errorMap: () => ({ message: "Please choose a gender" }),
     }),
     acceptTermsAndConds: z.literal(true, {
-      // message: "You must accept terms and conditions",
-      errorMap: (issue, ctx) => ({
+      errorMap: () => ({
         message: "You must accept terms and conditions",
       }),
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
+    password: z.string().min(6, {message:"Password must contain at least 6 characters"}).max(12, {message:"Password must not exceed 12 characters"}),
     confirmPassword: z.string(),
   })
   .refine(
-    //refine allows you check error in your own way
-    //in this example, we check "hasCoupon" with "coupon" fields
     (data) => {
-      // if user does not tick "I have coupon", then it's ok
       if (!data.hasCoupon) return true;
-
-      // if user tick "I have coupon" and fill correct code, then it's ok too
       if (data.hasCoupon && data.coupon === "CMU2023") return true;
-
-      // ticking "I have coupon" but fill wrong coupon code, show error
       return false;
     },
-    //set error message and the place it should show
     {
       message: "Invalid coupon code",
       path: ["coupon"],
+    }
+  ).refine(
+    (data) => {
+      if(data.confirmPassword != data.password) return false;
+      return true;
+    },
+    {
+      message: "Password does not match",
+      path: ["confirmPassword"],
     }
   );
 
@@ -96,6 +97,10 @@ export default function Home() {
     //TIP : get value of currently filled form with variable "form.values"
 
     if (form.values.plan === "funrun") price = 500;
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "half") price = 1200;
+    if (form.values.plan === "full") price = 1500;
+    if(form.values.hasCoupon && form.values.coupon === "CMU2023") price *= 0.7;
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
 
@@ -114,7 +119,7 @@ export default function Home() {
         <Space h="lg" />
 
         {/* add form */}
-        <form onSubmit={form.onSubmit((v) => alert("See you at CMU Marathon"))}>
+        <form onSubmit={form.onSubmit(() => alert("See you at CMU Marathon"))}>
           <Stack gap="sm">
             <Group grow align="start">
               <TextInput
@@ -187,7 +192,7 @@ export default function Home() {
           </Stack>
         </form>
 
-        {/* <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" /> */}
+        { <Footer year="2024" fullName="Supakorn Booranachart" studentId="660610798" />}
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
--- src/libs/types.ts
+++ src/libs/types.ts
@@ -4,5 +4,7 @@ export interface termsAndCondsProps {
 }
 
 export interface footerProps {
-  // define props here
+  year:string;
+  fullName:string;
+  studentId:string;
 }
